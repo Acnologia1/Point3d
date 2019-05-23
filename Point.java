612cf7c96660608c76c21d882bb6298eda000181
vancong <tavancong1994@gmail.com>
2019-05-23 08:50:43 +0700

    Create Point.java
    point

---
+++ Point.java
@@ -0,0 +1,48 @@
+package Lession4;
+
+public class Point {
+    public float x = 0.0f;
+    public float y = 0.0f;
+
+    public Point() {
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        float arr[] = new float[2];
+        arr[0] =x;
+        arr[1] =y;
+
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getX() + "," + getY() + ")";
+    }
+}
